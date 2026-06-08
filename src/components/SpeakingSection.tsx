36c2969ebf0fe432443f2f31156028b47bdfaf11
Fatima-weoveri <fatima@we-over-i.com>
2026-06-08 13:30:09 +0500

    Updated text

--- src/components/SpeakingSection.tsx
+++ src/components/SpeakingSection.tsx
@@ -58,7 +58,7 @@ function SpeakingIntro() {
             perspective, a greater sense of empowerment and action.
           </Text>
           <Text className="mb-4">
-            I&apos;ve worked with a range of companies, including Facebook, Imperial
+            I&apos;ve worked with a range of companies, including Meta, Imperial
             College London, Google, Tortoise, and Spotify.
           </Text>
 
